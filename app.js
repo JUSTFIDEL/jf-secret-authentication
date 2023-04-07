da6bc754b9be9a8f9a9976f531b95af7d1c744e6
JUSTFIDEL <fidenki1429@gmail.com>
2023-04-07 22:23:58 +0100

    Google STrategy Amendment

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ passport.use(
 		{
 			clientID: process.env.CLIENT_ID,
 			clientSecret: process.env.CLIENT_SECRET,
-			callbackURL: 'http://localhost:3000/auth/google/secrets',
+			callbackURL: 'https://jf-secret-app.onrender.com/auth/google/secrets',
 		},
 		function (accessToken, refreshToken, profile, cb) {
 			User.findOrCreate({ googleId: profile.id }, function (err, user) {
